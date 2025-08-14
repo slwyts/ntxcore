aa3b3258ecc29d1365eba262137df81397abc6c9
PixelYuki <slwyts@foxmail.com>
2025-08-14 21:13:06 +0800

    1

---
+++ admin/env.js
@@ -0,0 +1,4 @@
+// API Base URL
+//const API_BASE_URL = 'https://api.ntxdao.com/api'; // Production API URL
+const API_BASE_URL = 'https://api.devnet.ntxdao.com/api'; // Dev API URL
+//const API_BASE_URL = 'http://localhost:3000/api'; // Local development API URL
